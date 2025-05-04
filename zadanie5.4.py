a6ba3eb3efbcc92c1c6e0d4fd6c5caea4c210db2
roblig2 <rob.ligman@gmail.com>
2025-05-04 21:04:08 +0200

    remove media class

--- zadanie5.4.py
+++ zadanie5.4.py
@@ -2,13 +2,13 @@ import random
 from datetime import datetime
 
 
-class Media:
-    def __init__(self, title, year, genre, plays=0):
+class Movie:
+    def __init__(self, title, year, genre, director, plays=0):
         self.title = title
         self.year = year
         self.genre = genre
         self.plays = plays
-
+        self.director = director
     def play(self, count=1):
         self.plays += count
 
@@ -16,24 +16,22 @@ class Media:
         return f"{self.title} ({self.year})"
 
 
-class Movie(Media):
-    def __init__(self, title, year, genre, director, plays=0):
-        super().__init__(title, year, genre, plays)
+
+class Series:
+    def __init__(self, title, year, genre, season, episode, director, plays=0):
+        self.title = title
+        self.year = year
+        self.genre = genre
+        self.plays = plays
+        self.season = season
+        self.episode = episode
         self.director = director
 
     def __str__(self):
-        return f"{self.title} ({self.year}), Directed by {self.director}"
+        return f"{self.title} S{self.season:02}E{self.episode:02}, Directed by: {self.director}"
 
-
-class Series(Media):
-    def __init__(self, title, year, genre, season, episode, plays=0):
-        super().__init__(title, year, genre, plays)
-        self.season = season
-        self.episode = episode
-
-    def __str__(self):
-        return f"{self.title} S{self.season:02}E{self.episode:02}"
-
+    def play(self, count=1):
+        self.plays += count
 
 def get_movies(library):
     return sorted(
@@ -81,9 +79,9 @@ if __name__ == "__main__":
         Movie("Pulp Fiction", 1994, "Crime", "Quentin Tarantino"),
         Movie("Inception", 2010, "Sci-Fi", "Christopher Nolan"),
         Movie("The Godfather", 1972, "Crime", "Francis Ford Coppola"),
-        Series("The Simpsons", 1989, "Animation", 1, 5),
-        Series("Breaking Bad", 2008, "Drama", 2, 3),
-        Series("Stranger Things", 2016, "Horror", 3, 1),
+        Series("The Simpsons", 1989, "Animation", 1, 5, "James L. Brooks"),
+        Series("Breaking Bad", 2008, "Drama", 2, 3, "Michelle MacLaren"),
+        Series("Stranger Things", 2016, "Horror", 3, 1, "Matthew C. Duffer"),
     ]
 
     generate_views_multiple(library)
